a41ac0b6cf57bd0e4c3cfa1212593f66ba49726a
pylypchukroman <romanpylypchuk@gmail.com>
2026-06-03 19:16:03 +1000

    added some code styles

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,15 @@
 document.querySelectorAll('[data-modal-open]').forEach(function(btn) {
     btn.addEventListener('click', function() {
-        var id = btn.getAttribute('data-modal-open');
-        var modal = document.querySelector('[data-modal="' + id + '"]');
+        const id = btn.getAttribute('data-modal-open');
+        const modal = document.querySelector('[data-modal="' + id + '"]');
         if (modal) modal.classList.remove('is-hidden');
     });
 });
 
 document.querySelectorAll('[data-modal-close]').forEach(function(btn) {
     btn.addEventListener('click', function() {
-        var id = btn.getAttribute('data-modal-close');
-        var modal = document.querySelector('[data-modal="' + id + '"]');
+        const id = btn.getAttribute('data-modal-close');
+        const modal = document.querySelector('[data-modal="' + id + '"]');
         if (modal) {
             modal.classList.add('is-hidden');
             modal.querySelectorAll('.field-error').forEach(function(el) { el.remove(); });
